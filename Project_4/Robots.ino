cec213a7846e1a721c7cb222c8134f2d63964cf0
JeanineBray <6375151+JeanineBray@users.noreply.github.com>
2018-11-05 09:31:22 -0500

    Add files via upload

---
+++ Project_4/Robots.ino
@@ -0,0 +1,34 @@
+/*
+
+
+*/
+
+#include<iostream>
+
+//Define what pins are tied to what hardware
+//Define directions
+
+
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+	//Initialize all pins to assign software to hardware
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+	//Read sensor to detect direction
+	//Algorithmn for processing those directions
+
+}
